fc2ad14ec1272871402aa31aa6be11c0f5712067
Rech99 <rech_99@hotmail.com>
2021-03-09 10:41:02 -0700

    required

--- resources/views/agregar.blade.php
+++ resources/views/agregar.blade.php
@@ -10,10 +10,10 @@
                         <form method="POST" action="{{ url('crear') }}">
                             @csrf
                             <label for="title" class="field-label">Título: </label>
-                            <input type="text" name="title" id="title" class="field-input">
+                            <input type="text" name="title" id="title" class="field-input" required>
 
                             <label for="content" class="field-label">Contenido:</label>
-                            <textarea name="content" id="content" rows="10" class="field-textarea"></textarea>
+                            <textarea name="content" id="content" rows="10" class="field-textarea" required></textarea>
 
                             <button type="submit" class="btn btn-primary">Crear nota</button>
                         </form>
